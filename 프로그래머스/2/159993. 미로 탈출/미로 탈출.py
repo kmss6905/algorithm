7dffa98626f7bcb37de9d7ceba7515b3a06080bd
minshik <43781484+kmss6905@users.noreply.github.com>
2024-05-07 23:19:57 +0900

    [level 2] Title: 미로 탈출, Time: 0.05 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/159993. 미로 탈출/미로 탈출.py
@@ -0,0 +1,55 @@
+from collections import deque
+
+def findDestinationByType(maps, _type):
+    for i in range(len(maps)):
+        for j in range(len(maps[0])):
+            if maps[i][j] == _type:
+                return (i, j)
+            
+def bfs(srcx, srcy, src_cost, destinationType, maps):
+    queue = deque()
+    visited = [[-1] * len(maps[0]) for _ in range(len(maps))]
+    distinations = [(-1,0), (1,0), (0, 1), (0, -1)]
+    
+    queue.append((srcx, srcy, src_cost))
+    visited[srcx][srcy] = src_cost
+    
+    while queue:
+        cx, cy, cost = queue.popleft()
+        for dx, dy in distinations:
+            nx = cx + dx
+            ny = cy + dy
+            if 0 <= nx < len(maps) and 0 <= ny < len(maps[0]) and visited[nx][ny] == -1:
+                if maps[nx][ny] == destinationType:
+                    return (nx, ny, cost+1)
+                
+                if maps[nx][ny] != 'X' :
+                    visited[nx][ny] = cost + 1
+                    queue.append((nx, ny, cost+1))
+    return (0,0,-1)
+    
+    
+    
+    
+    
+    
+
+def solution(maps):
+    answer = 0
+    
+    m = len(maps)
+    n = len(maps[0])
+    
+    x, y = findDestinationByType(maps, 'S')
+    print(x, y)
+    lever_x, lever_y, lever_cost = bfs(x, y, 0, 'L', maps)
+    print(lever_x, lever_y, lever_cost)
+    if lever_cost == -1:
+        return -1
+    exit_x, exit_y, exit_cost = bfs(lever_x, lever_y, lever_cost, 'E', maps)
+    print(exit_x, exit_y, exit_cost)
+    return exit_cost
+                    
+    
+    
+                  
