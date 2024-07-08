71ae1940d6c2a3b1f734a34f92edba035da9efd1
minshik <43781484+kmss6905@users.noreply.github.com>
2024-07-08 17:04:01 +0900

    [level 3] Title: 베스트앨범, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
@@ -1,31 +1,94 @@
+"""
+genremap = {}
+playssum_geners = {}
+for len(geners) -> if genere[i] not in geners: -> gemerma[genere[i]] = [plays[i]], if playessum_gueners not in genere[i]
+playsum.geners[generp[i]] = plays[i]
+el: playsum.geners[generp[i]] += plays[i]
+                -> else: -> gemerma[genere[i]].append((i, plays[i]))
+                
+palysum_geners.sort 정렬 이후 
+palysum_geners.sort[0]
+palysum_geners.sort[1]
+
+palysum_geners.sort[0] -> genremap -> 찾기 -> 정렬 -> [0][1] 구하기
+
+"""
+
+
+# def solution(genres, plays):
+#     answer = []
+#     gm = {}
+#     gm_sum = {}
+#     for i in range(len(genres)):
+#         if genres[i] not in gm:
+#             gm_sum[genres[i]] = plays[i]
+#             gm[genres[i]] = [(i, plays[i])]
+#         else:
+#             gm_sum[genres[i]] += plays[i]
+#             gm[genres[i]].append((i, plays[i]))
+    
+#     print(gm)
+#     print(gm_sum)
+    
+#     if len(gm) == 1:
+#         for value in gm.values():
+#             l = sorted(value, reverse=True, key=lambda x:x[1])
+#             if len(l) >= 2:
+#                 ll = l[:2]
+#                 return [ll[0][0], ll[1][0]]
+#             else:
+#                 return [l[0][0]]
+            
+#     sorted_gen = sorted(gm_sum, reverse=True)
+#     l = sorted(gm[sorted_gen[0]], reverse=True, key=lambda x:x[1])
+#     r = sorted(gm[sorted_gen[1]], reverse=True, key=lambda x:x[1])
+    
+#     if len(l) == 1:
+#         answer.append(l[0][0])
+#     else:
+#         answer.append(l[0][0])
+#         answer.append(l[1][0])
+        
+#     if len(r) == 1:
+#         answer.append(r[0][0])
+#     else:
+#         answer.append(r[0][0])
+#         answer.append(r[1][0])
+    
+#     return answer
+
+
+
+
+
+
 def solution(genres, plays):
     answer = []
-    genres_plays_dic = {}
-    geners_plays_sum = {}
+    gm = {}
+    gm_sum = {}
+    
+    # Fill the dictionaries with genre information and play counts
     for i in range(len(genres)):
-        if genres[i] not in genres_plays_dic:
-            genres_plays_dic[genres[i]] = [(i, plays[i])]
-            geners_plays_sum[genres[i]] = plays[i]
+        if genres[i] not in gm:
+            gm[genres[i]] = [(i, plays[i])]
+            gm_sum[genres[i]] = plays[i]
         else:
-            genres_plays_dic[genres[i]].append((i, plays[i]))
-            geners_plays_sum[genres[i]] += plays[i]
+            gm[genres[i]].append((i, plays[i]))
+            gm_sum[genres[i]] += plays[i]
     
-    """
-    geners_plays_sum 딕셔너리의 각 항목을 재생 횟수(x[1])를 기준으로 내림차순으로 정렬합니다.
-    여기서 geners_plays_sum.items()는 딕셔너리의 키-값 쌍을 나타내는 (키, 값) 튜플의 리스트를 반환합니다.
-    key=lambda x: x[1] 부분은 각 튜플의 두 번째 요소, 즉 값에 대해 정렬을 수행합니다.
-    reverse=True는 역순으로 정렬한다는 것을 의미합니다. 즉, 재생 횟수가 큰 순서대로 정렬됩니다.
-    """
-    sorted_data = dict(sorted(geners_plays_sum.items(), key=lambda x: x[1], reverse=True))
+    # Sort genres by total plays in descending order
+    sorted_gen = sorted(gm_sum.keys(), key=lambda x: gm_sum[x], reverse=True)
     
-    for genres in sorted_data: # "classic"
-        sorted_genres = sorted(genres_plays_dic[genres], key=lambda x: x[1], reverse=True)
-
-        # 장르에 속한 곡이 하나 인 경우
-        if len(genres_plays_dic[genres]) == 1:
-            answer.append(sorted_genres[0][0])
-        else:
-            answer.append(sorted_genres[0][0])
-            answer.append(sorted_genres[1][0])
+    # For each genre, sort the songs by plays in descending order and add the top two to the answer
+    for genre in sorted_gen:
+        songs = sorted(gm[genre], key=lambda x: x[1], reverse=True)
+        answer.extend([song[0] for song in songs[:2]])
     
     return answer
+
+
+
+
+
+
+
