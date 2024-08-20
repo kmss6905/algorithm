d226a4b024b20ed0a3eb3bc3627784c2f42afa64
minshik <43781484+kmss6905@users.noreply.github.com>
2024-08-20 17:09:50 +0900

    [level 0] Title: 수 조작하기 2, Time: 31.88 ms, Memory: 13.9 MB -BaekjoonHub

--- 프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.py
+++ 프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.py
@@ -1,8 +1,11 @@
+"""
+joystick=dict(zip([1,-1,10,-10],['w','s','d','a']))
+    for i in range(1,len(log)):
+        res+=joystick[log[i]-log[i-1]]
+"""
 def solution(numLog):
     op = {'1': 'w', '-1':'s', '10': 'd', '-10': 'a'}
     answer = ''
-    for i in range(len(numLog)):
-        if i+1 < len(numLog):
-            answer += op[str(numLog[i+1] - numLog[i])]
-                
+    for i in range(1, len(numLog)):
+        answer += op[str(numLog[i] - numLog[i-1])]
     return answer
