66659ecdf8c6b623fa79d6a73fc83dd1f1353c86
minshik <43781484+kmss6905@users.noreply.github.com>
2024-08-20 14:28:18 +0900

    [level 0] Title: 문자열 뒤집기, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/181905. 문자열 뒤집기/문자열 뒤집기.py
+++ 프로그래머스/0/181905. 문자열 뒤집기/문자열 뒤집기.py
@@ -1,2 +1,19 @@
+# 파이썬 문자열 뒤집기
+"""
+1. for
+a = "hello world"
+c = ''
+result = ''
+for i in a:
+    result = i + c # 앞에다 계속 더하는 방법
+    
+
+2. list -> reverse -> join -> str 
+''.join(list(a).reverse())
+
+3. str[::-1]
+
+"""
+
 def solution(my_string, s, e):
     return my_string[:s] + my_string[s:e+1][::-1] + my_string[e+1:]
