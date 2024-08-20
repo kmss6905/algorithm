e2b02bb34d2229036820950d0a421fc31ec77dbb
minshik <43781484+kmss6905@users.noreply.github.com>
2024-08-20 11:04:37 +0900

    [level 0] Title: 문자열 잘라서 정렬하기, Time: 5.76 ms, Memory: 11.8 MB -BaekjoonHub

--- 프로그래머스/0/181866. 문자열 잘라서 정렬하기/문자열 잘라서 정렬하기.py
+++ 프로그래머스/0/181866. 문자열 잘라서 정렬하기/문자열 잘라서 정렬하기.py
@@ -1,17 +1,20 @@
 def solution(myString):
-    answer = []
-    tmpStr = ''
-    for idx, e in enumerate(list(myString)):
-        if e != 'x':
-            tmpStr += e
-            if idx == len(myString) - 1:
-                answer.append(tmpStr)
-                break
-            continue
+    return sorted([ch for ch in myString.split('x') if ch])
 
-        if tmpStr != '':
-            answer.append(tmpStr)    
-            tmpStr = ''
+# def solution(myString):
+#     answer = []
+#     tmpStr = ''
+#     for idx, e in enumerate(list(myString)):
+#         if e != 'x':
+#             tmpStr += e
+#             if idx == len(myString) - 1:
+#                 answer.append(tmpStr)
+#                 break
+#             continue
+
+#         if tmpStr != '':
+#             answer.append(tmpStr)    
+#             tmpStr = ''
     
-    answer.sort()
-    return answer
+#     answer.sort()
+#     return answer
