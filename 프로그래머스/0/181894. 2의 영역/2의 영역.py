f690edf29f439389fa7b6306675fa0ad335a18d4
minshik <43781484+kmss6905@users.noreply.github.com>
2024-08-26 16:01:39 +0900

    [level 0] Title: 2의 영역, Time: 8.80 ms, Memory: 15.1 MB -BaekjoonHub

--- 프로그래머스/0/181894. 2의 영역/2의 영역.py
+++ 프로그래머스/0/181894. 2의 영역/2의 영역.py
@@ -1,15 +1,13 @@
+#  arr[arr.index(2) : len(arr) - arr[::-1].index(2)]
 def solution(arr):
     result = []
+    
+    if 2 not in arr:
+        return [-1]
+    
     for i in range(len(arr)):
         if arr[i] == 2:
             result.append(i)
-    
-    if len(result) == 0:
-        return [-1]
-    
-    result = sorted(result)
-    answer = []
-    for i in range(result[0], result[-1]+1):
-        answer.append(arr[i])
-    
-    return answer
+
+    result.sort()
+    return arr[result[0]: result[-1]+1]
