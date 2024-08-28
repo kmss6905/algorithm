28cfa3ee356a158b08bb3ba5c0eb5e391b39adb8
minshik <43781484+kmss6905@users.noreply.github.com>
2024-08-28 21:14:08 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 53.68 ms, Memory: 34 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,19 +1,17 @@
+from collections import defaultdict
 
 def solution(participant, completion):
-    pm = {}
-    cm = {}
-    for i in participant:
-        if i not in pm:
-            pm[i] = 1
-        else:
-            pm[i] += 1
+    answer = ''
+    parti_dic = defaultdict(int)
+    for name in participant:
+        parti_dic[name] += 1
     
-    for i in completion:
-        if i not in cm:
-            cm[i] = 1
-        else:
-            cm[i] += 1
-    
-    for i in pm:
-        if i not in cm or cm[i] != pm[i]:
-            return i
+    for name in completion:
+        parti_dic[name] -= 1
+
+    for key, value in parti_dic.items():
+        if value != 0:
+            answer = key
+            break
+            
+    return answer
