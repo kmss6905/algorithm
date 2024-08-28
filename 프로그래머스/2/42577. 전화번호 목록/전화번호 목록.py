a4798b73ecbe539b972913e3b9d303cde91a68a0
minshik <43781484+kmss6905@users.noreply.github.com>
2024-08-28 21:52:05 +0900

    [level 2] Title: 전화번호 목록, Time: 461.56 ms, Memory: 46.8 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
@@ -1,8 +1,19 @@
-# 완전탐색으로는 불가능 -> 10^6C2 의 결과 10^8 초과
-# 정렬? -> 힌트? 
-def solution(phone_book): 
-    pb = sorted(phone_book)
-    for i in range(len(pb)):
-        if i < len(pb) - 1 and pb[i+1].startswith(pb[i]):
-            return False
-    return True
+def solution(phone_book):
+    answer = True
+    hash_map = {}
+    # O(N)
+    for phone in phone_book:
+        hash_map[phone] = 1
+
+    # O(N)
+    for phone in phone_book:
+        prefix = ''
+        # O(1) 
+        for i in phone: # 최대 문자 길이가 20
+            prefix += i
+            if prefix in hash_map  and prefix != phone:
+                return False
+    
+    # O(N) + O(20N) -> 20 000 000 < 10 ^ 8 => 시간복잡도 통과
+    
+    return answer
