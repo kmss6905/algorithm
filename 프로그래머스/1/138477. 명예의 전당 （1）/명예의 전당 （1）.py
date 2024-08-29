fd1bd98b8582f824a7f600ab01b092db6b182a49
minshik <43781484+kmss6905@users.noreply.github.com>
2024-08-30 00:43:28 +0900

    [level 1] Title: 명예의 전당 (1), Time: 95.12 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.py
+++ 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.py
@@ -1,17 +1,12 @@
-"""
-1. 명예의 전당을 관리하는 hq 
-2. score 반복하면서 명예의 전당에 계속 push, 명예의 전당의 수가 K보다 작은 경우에는 마지막 원소, 큰 경우에는 k번째 점수 추가
-"""
+
 import heapq
+
 def solution(k, score):
+    max_heap = []
     answer = []
-    temp = []
-    for i in range(len(score)):
-        temp.append(score[i])
-        temp = sorted(temp, reverse=True)
-        if len(temp) < k:
-            answer.append(temp[-1])
-        else:
-            answer.append(temp[k-1])
-    print(answer)
+
+    for sc in score:
+        heapq.heappush(max_heap, (-sc, sc))
+        answer.append(max(heapq.nsmallest(k, max_heap))[1])
+
     return answer
