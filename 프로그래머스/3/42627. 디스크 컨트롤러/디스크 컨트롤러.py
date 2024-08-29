2873a1bfe9238c18d04a992a64169ec3d6599cbf
minshik <43781484+kmss6905@users.noreply.github.com>
2024-08-29 16:48:36 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 1.11 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.py
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.py
@@ -1,20 +1,20 @@
 def solution(jobs):
     answer = 0
-    jobs_num = len(jobs)
-    jobs = sorted(jobs, key = lambda x : x[1]) # ex, [[2, 6], [1, 9]]
-    start = 0 # start = 3
-    
-    # 대기시간 + 작업시간
-    while len(jobs) != 0 :
-        for i in range (len(jobs)):
-            # 작업 요청 시점
-            if jobs[i][0] <= start: # 2 < 3 -> 어? 이미 요청이 됬다
-                start += jobs[i][1]  # 3 + 6 -> 9
-                answer += abs(jobs[i][0] - start) # 2 - 9
+    len_jobs = len(jobs)
+    jobs = sorted(jobs, key = lambda x : x[1])
+    start = 0
+    while jobs:
+        for i in range(len(jobs)):
+            if jobs[i][0] <= start:
+                worktime = jobs[i][1]
+                delaytime = abs(jobs[i][0] - start)
+                answer += (worktime + delaytime)
+                start += jobs[i][1]
                 jobs.pop(i)
-                break;
+                break            
+            
+            if i == len(jobs) - 1:
+                start += 1
+            
 
-            if i == len(jobs) -1 : # 마지막 도착시, start += 1
-                start +=1                
-
-    return answer // jobs_num
+    return answer // len_jobs
