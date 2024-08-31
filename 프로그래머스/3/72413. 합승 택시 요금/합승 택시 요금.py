220e869c44a264af18972dd1ddc66f18a78c5833
minshik <43781484+kmss6905@users.noreply.github.com>
2024-08-31 13:49:34 +0900

    [level 3] Title: 합승 택시 요금, Time: 22.27 ms, Memory: 15.5 MB -BaekjoonHub

--- 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.py
+++ 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.py
@@ -1,38 +1,40 @@
 import heapq
 
 def solution(n, s, a, b, fares):
+    # 그래프 초기화
     graph = [[] for _ in range(n+1)]
     for c, d, f in fares:
         graph[c].append((f, d))
         graph[d].append((f, c))
     
-    def dijkstra(start):
-        MAX = 100000 * 200
+    # 다익스트라 알고리즘 함수 정의
+    def dijkstra(start, graph):
+        MAX = 1000000 * n
         dist = [MAX] * (n+1)
         dist[start] = 0
+        hq = [(0, start)]
         
-        hq = []
-        heapq.heappush(hq, (0, start))
-        
-        visited = [False] * (n+1)
         while hq:
             cur_cost, cur_node = heapq.heappop(hq)
             
             if dist[cur_node] < cur_cost:
                 continue
-            
+                   
             for next_cost, next_node in graph[cur_node]:
-                total_cost = cur_cost + next_cost
+                total_cost = next_cost + cur_cost
                 if total_cost < dist[next_node]:
                     dist[next_node] = total_cost
                     heapq.heappush(hq, (total_cost, next_node))
         return dist
     
-    distance = dijkstra(s)
-    res = distance[a] + distance[b]
+    # s, a, b에서 각각 다익스트라 실행
+    dist_s = dijkstra(s, graph)
+    dist_a = dijkstra(a, graph)
+    dist_b = dijkstra(b, graph)
+    
+    # 최소 비용 계산
+    answer = float('inf')
     for i in range(1, n+1):
-        if i == s:
-            continue
-        point_dist = dijkstra(i)
-        res = min(point_dist[a]+point_dist[b]+distance[i], res)
-    return res
+        answer = min(answer, dist_s[i] + dist_a[i] + dist_b[i])
+    
+    return answer
