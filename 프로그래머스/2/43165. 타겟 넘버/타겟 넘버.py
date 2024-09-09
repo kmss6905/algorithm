8c5d23e3dc6405b7008d5e0375185a94abf79db6
minshik <43781484+kmss6905@users.noreply.github.com>
2024-09-09 10:45:24 +0900

    [level 2] Title: 타겟 넘버, Time: 724.20 ms, Memory: 99.8 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,13 +1,30 @@
+"""
+1. 아이디어
+BFS
+2. 시간복잡도
+2^20
+3. 구현
+ops = [1, -1]
+
+"""
+from collections import deque
+
 def solution(numbers, target):
     answer = 0
-    leaves = [0]
-    for num in numbers:
-        tmp = []
-        for parent in leaves:
-            tmp.append(parent + num)
-            tmp.append(parent - num)
-        leaves = tmp
-    for leaf in leaves:
-        if leaf == target:
-            answer += 1
+    q = deque()
+    q.append((0, numbers[0]))
+    q.append((0, -numbers[0]))
+    ops = [-1, 1]
+    while q:
+        idx, n = q.popleft()
+        if idx == len(numbers) - 1:
+            if n == target:
+                answer += 1
+            continue
+
+        idx += 1
+        for op in ops:
+            next_n = n + (numbers[idx] * op)
+            q.append((idx, next_n))
+    
     return answer
