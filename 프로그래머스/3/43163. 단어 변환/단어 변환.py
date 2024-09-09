347ac40bde87d57dea3a9e1590f4677e9a799f37
minshik <43781484+kmss6905@users.noreply.github.com>
2024-09-09 13:56:39 +0900

    [level 3] Title: 단어 변환, Time: 2.38 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.py
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.py
@@ -2,41 +2,35 @@ from collections import deque
 
 def solution(begin, target, words):
     answer = 0
-    
-    # 무조건 변환할 수 없는 경우
-    if target not in words:
-        return answer
-    
-    # 그래프 구현
-    graph = {}
     words.append(begin)
-    word_len = len(words[0])
-    for w in words:
-        if w not in graph:
-            graph[w] = []
-        
-        for other_word in words:
-            diff_alpha_cnt = 0
-            if w != other_word:
-                for i in range(word_len):
-                    if w[i] != other_word[i]:
-                        diff_alpha_cnt += 1
-            if diff_alpha_cnt == 1:
-                graph[w].append(other_word)
-                
-    # 그래프 탐색
-    visited = []
-    queue = deque()
-    queue.append((begin, 0))
-    visited.append(begin)
-    while queue:
-        cur_word, distance = queue.popleft()
-        for next_node in graph[cur_word]:
-            if next_node not in visited:
-                distance += 1
-                visited.append(next_node)
-                queue.append((next_node, distance))
-                if next_node == target:
-                    return distance
-        
+    if target not in words:
+        return 0
+    
+    graph = {}
+    visited = {}
+    for word in words:
+        graph[word] = []
+        visited[word] = False
+    
+    for key in graph.keys():
+        for word in words:
+            cnt = 0
+            if key != word:
+                for i in range(len(key)):
+                    if key[i] == word[i]:
+                        cnt += 1
+                if cnt == len(key) - 1:
+                    graph[key].append(word)
+
+    q = deque()
+    q.append((0, begin))    
+    while q:
+        cnt, current_word = q.popleft()
+        if current_word == target:
+            answer = cnt
+            break
+        for next_word in graph[current_word]:
+            if not visited[next_word]:
+                q.append((cnt+1, next_word))
+                visited[next_word] = True
     return answer
