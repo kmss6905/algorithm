217e2dae6120a74ae002c57372dceb27f03aaaba
mcode <43781484+kmss6905@users.noreply.github.com>
2024-09-12 18:31:09 +0900

    [level 2] Title: 연속된 부분 수열의 합, Time: 376.93 ms, Memory: 43.2 MB -BaekjoonHub

--- 프로그래머스/2/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.py
+++ 프로그래머스/2/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.py
@@ -1,17 +1,27 @@
 def solution(sequence, k):
+    # 투포인터 각각 가리킬 왼,오 인덱스 선언
     l = r = 0
+    
+    # 최대 가장 긴 경우
     answer = [0, len(sequence)]
     sum = sequence[0]
 
     while True:
+        # 합이 k 보다 작은 경우
         if sum < k:
+            # 우측으로 이동하면서 누적합 구함
             r += 1
             if r == len(sequence): break
             sum += sequence[r]
+        # 합이 k 보다 작거나 같은 경우
         else:
             if sum == k:
+                # 배열의 원소 개수가 최소인 것만 추가한다.
                 if r-l < answer[1]-answer[0]:
                     answer = [l, r]
+            # sum >= k 무조건 sum 은 갱신되어야한다. 그렇게 해야 모든 경우 탐색 가능
             sum -= sequence[l]
+            
+            # 왼쪽에서 증가
             l += 1
     return answer
