379dfe1b79c16cedc54b1b2657af3c575af26f2a
mcode <43781484+kmss6905@users.noreply.github.com>
2024-09-13 12:53:11 +0900

    [Silver II] Title: 랜선 자르기, Time: 280 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1654. 랜선 자르기/랜선 자르기.py
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.py
@@ -1,26 +1,32 @@
 def solutions():
     K, target = map(int, input().split())
     lines = []
+    # O (N)
     for _ in range(K):
         lines.append(int(input()))
-    start, end = 1, max(lines)  # 최소 1부터 최대 라인의 길이까지
+    start, end = 1, max(lines)
 
+    # 자를 때 마다 O(N) , N(K) = 10,000
     def cut(length):
         total = 0
         for line in lines:
             total += line // length
         return total
 
+    # O(k log N)
+    # 문제에서는 최대의 길이를 물어봤기 때문에
     answer = 0
     while start <= end:
         mid = (start + end) // 2
-        if cut(mid) >= target:  # 잘라낸 개수가 target보다 크거나 같다면
-            answer = mid  # 일단 현재 mid 값은 가능한 답
-            start = mid + 1  # 더 큰 길이를 시도해 본다
+        if cut(mid) >= target:  # 좀 더 크게 잘라본다.
+            answer = mid
+            start = mid + 1
         else:  # 잘라낸 개수가 target보다 작다면
-            end = mid - 1  # 길이를 줄여본다
-
+            end = mid - 1
     return answer
+
 
 if __name__ == '__main__':
     print(solutions())
+
+
