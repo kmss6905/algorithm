9eb2be730648cdf858096efc6ab46b4ed435568e
mcode <43781484+kmss6905@users.noreply.github.com>
2024-09-25 18:29:54 +0900

    [Bronze II] Title: 소수 찾기, Time: 28 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/1978. 소수 찾기/소수 찾기.py
+++ 백준/Bronze/1978. 소수 찾기/소수 찾기.py
@@ -1,14 +1,16 @@
-a = input()
-b = list(map(int, input().split()))
-cnt = 0 # 소수가 아닌 것들
-for i in b:
-    tmp = i
-    if tmp == 1:
-        cnt += 1
-        continue
-    while tmp != 2:
-        tmp -= 1
-        if i % tmp == 0:
-            cnt += 1
-            break
-print(len(b)-cnt)
+def sol():
+    n = int(input())
+    m = map(int, input().split())
+    answer = 0
+    for i in m:
+        num = 0
+        for j in range(1, i+1):
+            if i % j == 0:
+                num += 1
+
+        if num == 2:
+            answer += 1
+    return answer
+
+if __name__ == '__main__':
+    print(sol())
