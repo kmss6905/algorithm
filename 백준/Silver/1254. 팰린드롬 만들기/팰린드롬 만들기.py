69a730ea08006c9751a2cf8a11dfb774b2d91ec0
mcode <43781484+kmss6905@users.noreply.github.com>
2024-09-30 16:33:10 +0900

    [Silver II] Title: 팰린드롬 만들기, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1254. 팰린드롬 만들기/팰린드롬 만들기.py
+++ 백준/Silver/1254. 팰린드롬 만들기/팰린드롬 만들기.py
@@ -1,8 +1,14 @@
-a = input()
-al = len(a)
-for i in range(len(a)):
-    b = a[i::]
-    c = b[::-1]
-    if b == c:
-        print(al+i)
-        break
+def solutions():
+    a = input()
+    al = len(a)  # O(1)
+    # O(N^2)
+    for i in range(len(a)):  # O(N)
+        b = a[i::]  # O(N)
+        c = b[::-1]  # O(N)
+        if b == c:
+            print(al+i)
+            break
+    
+
+if __name__ == '__main__':
+    solutions()
