2c2051687a83cf8957374267e05ad6017a828df2
mcode <43781484+kmss6905@users.noreply.github.com>
2024-10-07 16:54:14 +0900

    [level 1] Title: 폰켓몬, Time: 1.12 ms, Memory: 10.9 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
@@ -1,3 +1,15 @@
+"""
+1. 아이디어
+SET 함수를 통한 종류만 추출
+
+2. 
+33 2 1 55
+1235
+
+3
+
+len(set(nums)) if nums/2 < len(set(nums)) else nums/2
+
+"""
 def solution(nums):
-    sel = len(nums) // 2
-    return min(len(set(nums)), sel)
+    return len(set(nums)) if len(nums)/2 >= len(set(nums)) else len(nums)/2
