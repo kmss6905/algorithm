80bfca9a6df2fdccbf0f4ec272f2b664bf4c10b9
mcode <43781484+kmss6905@users.noreply.github.com>
2024-10-07 17:43:22 +0900

    [level 2] Title: 전화번호 목록, Time: 474.31 ms, Memory: 34.6 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
@@ -1,19 +1,12 @@
 def solution(phone_book):
     answer = True
     hash_map = {}
-    # O(N)
-    for phone in phone_book:
-        hash_map[phone] = 1
-
-    # O(N)
-    for phone in phone_book:
-        prefix = ''
-        # O(1) 
-        for i in phone: # 최대 문자 길이가 20
-            prefix += i
-            if prefix in hash_map  and prefix != phone:
-                return False
-    
-    # O(N) + O(20N) -> 20 000 000 < 10 ^ 8 => 시간복잡도 통과
-    
+    for phone_number in phone_book:
+        hash_map[phone_number] = 1
+    for phone_number in phone_book:
+        temp = ""
+        for number in phone_number:
+            temp += number
+            if temp in hash_map and temp != phone_number:
+                answer = False
     return answer
