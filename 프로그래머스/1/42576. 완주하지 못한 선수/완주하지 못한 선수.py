d069811ca966dcdfa5377cf3696127437c085543
mcode <43781484+kmss6905@users.noreply.github.com>
2024-10-07 17:01:49 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 44.17 ms, Memory: 33.9 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,17 +1,33 @@
-from collections import defaultdict
+"""
+1. 아이디어
+해시테이블, key-value 저장
 
+2. 구현
+participant -> 딕셔너리 저장 
+key 선수이름, value 는 +1
+
+completion 돌면서 해당되는 key 없으면 해당 key return
+있는 경우에는 에 -1 씩 함. 
+
+전부 돌고 최종 결과에서 value 가 1이 있인 key(참가자 이름) return
+
+"""
 def solution(participant, completion):
     answer = ''
-    parti_dic = defaultdict(int)
-    for name in participant:
-        parti_dic[name] += 1
+    _map = {}
+    for person in participant:
+        if person not in _map:
+            _map[person] = 1
+        else:
+            _map[person] += 1
     
-    for name in completion:
-        parti_dic[name] -= 1
-
-    for key, value in parti_dic.items():
-        if value != 0:
-            answer = key
-            break
-            
-    return answer
+    for person in completion:
+        if person not in _map:
+            answer = person
+            return person
+        _map[person] -= 1
+    
+    # check
+    for person in _map:
+        if _map[person] == 1:
+            return person
