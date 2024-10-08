ebd06452a7233e7b7a51cf0a80ebc7006195a3f1
mcode <43781484+kmss6905@users.noreply.github.com>
2024-10-08 17:34:54 +0900

    [level 2] Title: 의상, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.py
+++ 프로그래머스/2/42578. 의상/의상.py
@@ -1,14 +1,20 @@
+"""
+종류 별로 옷의 개수를 해시테이블에 저장한 후
+모든 옷 개수를 곱한 결과에서 1뺀다.
+어차피 30개 이하이기 때문에 시간복잡도 굳이 고려할 필요는 없다.
+clothes 의 길이 30
+
+"""
 def solution(clothes):
-    # 옷의 종류별 옷 개수
+    answer = 1
     clothes_map = {}
-    for clothe in clothes:
-        if clothe[1] not in clothes_map:
-            clothes_map[clothe[1]] = 1
+    for cloth, cloth_type in clothes:
+        if cloth_type not in clothes_map:
+            clothes_map[cloth_type] = 2
         else:
-            clothes_map[clothe[1]] += 1
-    result = 1
-    for k in clothes_map:
-        result *= (clothes_map[k] + 1)
+            clothes_map[cloth_type] += 1
     
-    answer = result - 1
+    for value in clothes_map.values():
+        answer *= value
+    answer -= 1
     return answer
