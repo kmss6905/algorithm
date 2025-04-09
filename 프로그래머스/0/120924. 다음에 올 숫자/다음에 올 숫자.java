343ddb97ae8743fe7b53d7c1d2d3be5c66a5b9ae
minshik <43781484+kmss6905@users.noreply.github.com>
2025-04-09 11:29:51 +0900

    [level 0] Title: 다음에 올 숫자, Time: 0.03 ms, Memory: 76.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/120924. 다음에 올 숫자/다음에 올 숫자.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int solution(int[] common) {
+        if(common[2] - common[1] == common[1] - common[0]){
+            return common[common.length - 1] + common[1] - common[0];
+        }
+        return common[common.length - 1] * (common[1] / common[0]);
+    }
+}
